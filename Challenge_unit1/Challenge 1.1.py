7e5187fd3c9fc4ef392a0d49b7de654dbb36eb45
vinish1234 <144550345+vinish1234@users.noreply.github.com>
2023-09-19 13:27:29 +0530

    Add files via upload

---
+++ Challenge_unit1/Challenge 1.1.py
@@ -0,0 +1,8 @@
+# online python compiler (interpreter) to run python online
+# Write python 3 code in this online editor and run it
+def fact(n):
+     if n==0 or n==1:
+        return 1
+     else:
+        return n* fact(n)
+print(fact(5))
---
+++ Challenge_unit1/Challenge 1.2.py
@@ -0,0 +1,7 @@
+#online python comptler (Interpreter) ro run python online.
+#write python 3 code in this online editor and run it.
+year = 2023
+if(year%4 == 0):
+  Print("Leap year")
+else:
+  print("Not Leap year")
